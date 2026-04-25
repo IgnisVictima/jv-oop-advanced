09234c8ad4adbe8d5868b810219c7fece075d00c
Vitalii Smuraha <smuraga.v@gmail.com>
2026-04-25 22:16:27 +0200

    added constants

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    
-    String[] colors = new String[] {"blue", "yellow", "red",  "black",  "pink",  "orange", "white"}; 
 
-    Random random = new Random(); 
+    private static String[] COLORS = new String[] { "blue", "yellow", "red", "black", "pink", "orange", "white" };
+
+    private Random random = new Random();
 
     public String getRandomColor() {
 
-        return colors[random.nextInt(colors.length)];
+        return COLORS[random.nextInt(COLORS.length)];
 
     }
 
